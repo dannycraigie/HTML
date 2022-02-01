37e31c82758f8c8645d45ea22ef6cac777c8b643
Danny Craigie <dannycraigie@outlook.com>
2022-02-01 10:38:05 +0000

    added first javascript exercises

---
+++ script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+let first_name = "Danny";
+let last_name = "Craigie";
+let area = "Manchester";
+let star_sign = "Libra";
+
+console.log(first_name);
+console.log(last_name);
+console.log(area);
+console.log(star_sign);
+
+console.log("This is a %c test formatting message with some CSS."+first_name, "color: orange; font-style: bold; background-color: black; padding: 10px; fontfamily: fantasy:");
+
+
